68e8393c70d8246a193852b3a79d69051be69b00
Aniketmadame05 <aniketmadame2021@gmail.com>
2021-08-14 00:05:47 +0530

    prime num

---
+++ Primenum.java
@@ -0,0 +1,34 @@
+//input a number and print whether it is prime or not
+package com.aniket;
+
+import java.util.Scanner;
+
+public class Primenum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        if(num<=1)
+        {
+            System.out.println("nor prime neither composite");
+
+        }else {
+            int i = 2;
+
+            boolean isPrime = true;
+
+            while (i <= num/2) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+                i++;
+            }
+            if (isPrime) {
+                System.out.println("it is a prime number");
+            } else {
+                System.out.println("not prime");
+            }
+        }
+
+    }
+}
